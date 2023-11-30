afe8e956fb6723d27281c3c92468dd3982de1609
tatoclemente <tatoapuros@gmail.com>
2023-11-30 12:42:54 -0300

    Created folder structures

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const Routes = require('./routes')
+
+const server = express()
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+  
+server.use(Routes)
+
+server.use(express.json())
+
+module.exports = server
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const server = require('./app')
+require('dotenv').config()
+
+const PORT = process.env.PORT | 3000
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
