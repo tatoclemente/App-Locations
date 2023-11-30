2fb0a6ba207ad75286f5f48848ca5453f60199cd
tatoclemente <tatoapuros@gmail.com>
2023-11-30 18:25:56 -0300

    Add routes PUT GET:id y DELETE

--- src/routes/person.router.js
+++ src/routes/person.router.js
@@ -2,52 +2,97 @@ const express = require('express')
 const { Person } = require('../db')
 const personRouter = express.Router()
 
-personRouter.get('/person', async (req, res) => {
-    const { name } = req.query
-  console.log("NAME: ", name);
-    if (!name) {
-      const allPersons = await Person.findAll()
-  
-      if (allPersons.length === 0) return res.status(400).json({ message: "No hay usuarios" })
-      res.status(200).json(allPersons)
-    } else {
-      const personsByName = await Person.findAll({
-        where: {
-          name: {
-            [Op.iLike]: `%${name}%`
-          }
-        }
-      })
 
-      res.status(200).send(personsByName)
-  
-    }
-  })
-  
+personRouter.get('/person/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const person = await Person.findByPk(id)
+    if (!person) res.status(400).json({ message: "La persona no existe" })
+    else res.status(200).json(person)
+  } catch (error) {
+    return res.status(400).json(error.message)
+  }
+})
+
+personRouter.get('/person', async (req, res) => {
+  const { name } = req.query
+
+  if (!name) {
+    const allPersons = await Person.findAll()
+
+    if (allPersons.length === 0) res.status(400).json({ message: "No hay usuarios" })
+    else res.status(200).json(allPersons)
+
+  } else {
+    const personsByName = await Person.findAll({
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`
+        }
+      }
+    })
+
+    res.status(200).send(personsByName)
+
+  }
+})
+
 
 personRouter.post('/person', async (req, res) => {
-    const { name, lastName, email, phone, address } = req.body
-  
-    try {
-      if (!name || !lastName || !phone || !address) return res.status(400).json({message: 'Faltan datos'})
-  
-  
-     const userData = { 
-        name,
-        lastName,
-        email: email || null,
-        phone,
-        address,
-      }
-      const [person, created] = await Person.findOrCreate({ where: userData })
-  
-      if (created === false) res.status(400).json({ message: "La persona ya existe" })
-      if (created === true) res.status(201).json(person)
-  
-    } catch (error) {
-      return res.status(400).json(error.message)
-    }
-  
-  })
+  const { name, lastName, email, phone, address } = req.body
 
-  module.exports = personRouter
+  try {
+    if (!name || !lastName || !phone || !address) res.status(400).json({ message: 'Faltan datos' })
+
+
+    const userData = {
+      name,
+      lastName,
+      email: email || null,
+      phone,
+      address,
+    }
+    const [person, created] = await Person.findOrCreate({ where: userData })
+
+    if (created === false) res.status(400).json({ message: "La persona ya existe" })
+    if (created === true) res.status(201).json(person)
+
+  } catch (error) {
+    return res.status(400).json(error.message)
+  }
+
+})
+
+personRouter.put('/person/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, lastName, email, phone, address, geoCoding, profileImg, password } = req.body
+
+  try {
+    const person = await Person.findByPk(id)
+
+    if (!person) res.status(400).json({ message: "La persona no existe" })
+
+    if (name || lastName || phone || address || geoCoding || profileImg || password) {
+      await person.update({ name, lastName, email, phone, address, geoCoding, profileImg, password })
+
+      return res.status(200).json(person)
+    }
+
+  } catch (error) {
+    return res.status(400).json(error.message)
+  }
+})
+
+personRouter.delete('/person/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const person = await Person.findByPk(id)
+    if (!person) return res.status(400).json({ error: "La persona no existe" })
+    await person.destroy()
+    res.status(200).json({ message: "Persona eliminada" })
+  } catch (error) {
+    return res.status(400).json(error.message)
+  }
+})
+
+module.exports = personRouter
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,38 +4,57 @@ const { Op } = require('sequelize')
 
 const userRouter = express.Router()
 
-userRouter.get('/user', async (req, res) => {
-  const { name } = req.query
-console.log("NAME: ", name);
-  if (!name) {
-    const allUsers = await User.findAll()
 
-    if (allUsers.length === 0) return res.status(400).json({ message: "No hay usuarios" })
-    res.status(200).json(allUsers)
-  } else {
-    const usersByName = await User.findAll({
-      where: {
-        name: {
-          [Op.iLike]: `%${name}%`
-        }
-      }
-    })
-    console.log(usersByName);
-    res.status(200).send(usersByName)
-
+// GET by ID
+userRouter.get('/user/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await User.findByPk(id)
+    if (!user) return res.status(400).json({ error: "El usuario no existe" })
+    res.status(200).json(user)
+  } catch (error) {
+    return res.status(400).json(error.message)
   }
+
 })
 
 
+// GET all Users or by name (with query)
+userRouter.get('/user', async (req, res) => {
+  const { name } = req.query
+
+  try {
+    if (!name) {
+      const allUsers = await User.findAll()
+  
+      if (allUsers.length === 0) return res.status(400).json({ message: "No hay usuarios" })
+      res.status(200).json(allUsers)
+    } else {
+      const usersByName = await User.findAll({
+        where: {
+          name: {
+            [Op.iLike]: `%${name}%`
+          }
+        }
+      })
+      console.log(usersByName);
+      res.status(200).send(usersByName)
+  
+    }
+  } catch (error) {
+    res.status(400).json(error.message)
+  }
+})
+
+// CREATE user whit email and password (required) , The route allows receiving first and last name
 userRouter.post('/user', async (req, res) => {
-  const { name, lastName, email, password, role } = req.body
+  const { name, lastName, email, password } = req.body
 
   try {
     if (!email || !password) return res.status(400).json({message: 'Faltan datos'})
 
 
    const userData = { 
-      role: role || 'user',
       name: name || null, 
       lastName: lastName || null,
       email, 
@@ -76,6 +95,19 @@ userRouter.put('/user/:id', async (req, res) => {
     return res.status(400).json(error.message)
   }
 
+})
+
+
+userRouter.delete('/user/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await User.findByPk(id)
+    if (!user) return res.status(400).json({ error: "El usuario no existe" })
+    await user.destroy()
+    res.status(200).json({ message: "Usuario eliminado" })
+  } catch (error) {
+    return res.status(400).json(error.message)
+  }
 })
 
 module.exports = userRouter
